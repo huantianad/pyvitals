5997943e5bef8913f691fb5a62a0d608b056cee4
huantian <davidtianli@gmail.com>
2021-07-19 16:34:20 -0700

    Rename async to async_main

--- pyvitals/__init__.py
+++ pyvitals/__init__.py
@@ -1,3 +1,3 @@
-from .main import (download_level, get_setlists_url, get_sheet_data,
-                   parse_level, parse_rdzip, parse_url,
-                   unzip_level, get_filename, get_filename_from_url)
+from .async_main import adownload_level, aget_filename_from_url, aget_setlists_url, aget_sheet_data, aparse_url
+from .main import (download_level, get_filename, get_filename_from_url, get_setlists_url, get_sheet_data, parse_level,
+                   parse_rdzip, parse_url, unzip_level)
