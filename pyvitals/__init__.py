d7e14503f2a4a0d8db6b4164658351e67d10800c
huantian <davidtianli@gmail.com>
2021-08-07 18:55:51 -0700

    Expose exceptions!

--- pyvitals/__init__.py
+++ pyvitals/__init__.py
@@ -2,3 +2,4 @@ from .async_main import (async_download_level, async_get_filename_from_url, asyn
                          async_get_sheet_data, async_parse_url)
 from .main import (download_level, get_filename, get_filename_from_url, get_setlists_url, get_sheet_data, parse_level,
                    parse_rdzip, parse_url, rename, unzip_level)
+from .exceptions import BadRDZipFile, BadURLFilename, BaseError
