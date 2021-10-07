92a3b4f8f86d5c3f7b6e3f77bd9d0986a4c6d298
huantian <davidtianli@gmail.com>
2021-10-06 18:41:28 -0700

    Actually expose No2PLevel error

--- pyvitals/__init__.py
+++ pyvitals/__init__.py
@@ -1,5 +1,5 @@
 from .async_main import (async_download_level, async_download_unzip, async_get_filename_from_url,
                          async_get_setlists_url, async_get_sheet_data, async_parse_url)
-from .exceptions import BadRDZipFile, BadURLFilename, BaseError
+from .exceptions import BadRDZipFile, BadURLFilename, No2PLevel, BaseError
 from .main import (download_level, download_unzip, get_filename, get_filename_from_url, get_setlists_url,
                    get_sheet_data, parse_level, parse_rdzip, parse_url, rename, unzip_level)
--- pyvitals/exceptions.py
+++ pyvitals/exceptions.py
@@ -14,7 +14,7 @@ class BadRDZipFile(BaseError, BadZipFile):
     Raised when a rdzip file is incorrectly zipped, or unable to be unzipped.
     """
 
-    def __init__(self, message: str, file_path: Path) -> None:  # TODO: better path type hint
+    def __init__(self, message: str, file_path: Path) -> None:
         self.message = message
         self.file_path = file_path
         super().__init__(self.message)
